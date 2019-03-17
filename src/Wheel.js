5728cd12cfe696ee9d18eb49d81ffc0c3a005d4e
Tom <tomwestmacott@hotmail.com>
2019-03-17 18:56:24 +0000

    Util methods for vectors to avoid mutation risk

--- src/Wheel.js
+++ src/Wheel.js
@@ -8,34 +8,22 @@ class Wheel {
         this.springRate = springRate * 10; // kg/m > N/m
         this.damperRate = damperRate;
     }
-    // direction = () => this.parent.pose.direction.clone();
-    
-    // up = () =>  this.parent.pose.up.clone();
     
     getRay = () => {
         const carPose = this.parent.pose;
-        const longOffset = carPose.direction.clone().multiplyScalar(this.longOffset);
-        const latOffset = this.parent.rightVector().multiplyScalar(this.latOffset);
-        const origin = carPose.position.clone().add(longOffset).add(latOffset).add(carPose.up);
-        const direction = carPose.up.clone().multiplyScalar(-1);
+        const longOffset = scale(carPose.direction, this.longOffset);
+        const latOffset = scale(this.parent.rightVector(), this.latOffset);
+        const origin = sum(carPose.position, longOffset, latOffset, carPose.up);
+        const direction = scale(carPose.up, -1);
         return new THREE.Raycaster(origin, direction);
     }
+    
     heightAboveGround = (meshes) => {
         const ray = this.getRay();
         const intersections = meshes.map(mesh => ray.intersectObject(mesh)).flat(2);
         return intersections.slice(0,1);  // first intersection or nothing
-        // const distances = intersections.map(i => i.distance - 1);  // Ray starts from 1m above the 'wheel' position to avoid falling through
-        // console.log(distances)
-        return firstOr(1000)(distances);
     }
-    // rightVector = () => this.direction().cross(this.up());
       
-    angleFrom = (v1, v2, aroundAxis) => {
-        const angle = v1.angleTo(v2);
-        const cross = v1.clone().cross(v2);
-        const polarity = Math.sign(aroundAxis.clone().dot(cross));
-        return angle * polarity; // angleTo is absolute, doesn't include direction (sigh)
-    }
     getForce = (meshes, steeringAngle, netAppliedTorque, time) => {
 
         const force = new THREE.Vector3(0, 0, 0);
@@ -49,11 +37,9 @@ class Wheel {
         const height = d - 1;
 
         const up = face.normal;  // normal of ground at contact point
-        const direction = this.parent.pose.direction.clone().applyAxisAngle(up, steeringAngle);  // car heading plus any steering lock
-        const right = direction.clone().cross(up); // should point along wheel 'axle'
+        const direction = rotateByAround(this.parent.pose.direction, steeringAngle, up);  // car heading plus any steering lock
+        const right = cross(direction, up); // should point along wheel 'axle'
 
-        // console.log(up, direction, right);
-        
         const displacement = this.travel - height;        
 
         // spring + damper
@@ -65,27 +51,25 @@ class Wheel {
             const damperForce = this.damperRate * verticalWheelRate;
             verticalForce += damperForce;
         } 
-        force.add(up.clone().multiplyScalar(verticalForce));
+        force.add(scale(up, verticalForce));
         this.prevDisplacement = this.lastDisplacement;
         this.lastDisplacement = displacement;
         
 
-        // console.log("displacement", displacement, "springForce", springForce)
-
         // brake and acceleration
         const availableTraction = verticalForce * this.parent.tyreGrip;
         const longitudinalForce = Math.min(netAppliedTorque, availableTraction);
-        force.add(direction.clone().multiplyScalar(longitudinalForce));
+        force.add(scale(direction, longitudinalForce));
 
         // lateral
 
         const spinVelocity = -this.longOffset * this.parent.yawRate;
-        const wheelVelocity = this.parent.velocity.clone().add(this.parent.rightVector().multiplyScalar(spinVelocity));
-        const slipAngle = this.angleFrom(wheelVelocity, direction, up);
+        const wheelVelocity = sum(this.parent.velocity, scale(this.parent.rightVector(), spinVelocity));
+        const slipAngle = angleFrom(wheelVelocity, direction, up);
        
         const slipForce = slipAngle * wheelVelocity.length() * this.parent.tyreSharpness;
         const lateralForce = Math.min(slipForce, availableTraction);
-        force.add(right.clone().multiplyScalar(-lateralForce));
+        force.add(scale(right, -lateralForce));
         const wheelIsSlipping = slipForce > availableTraction;
 
         return { force, up, wheelIsSlipping, height }; 
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,18 @@
 const firstOr = (otherwise) => (list) => list.length > 0 ? list[0] : otherwise;
+
+const scale = (vector, scalar) => vector.clone().multiplyScalar(scalar);
+
+const sum = (...vectors) => vectors.reduce((a, b) => a.clone().add(b));
+
+const cross = (vectorA, vectorB) => vectorA.clone().cross(vectorB);
+
+const dot = (vectorA, vectorB) => vectorA.clone().dot(vectorB);
+
+const rotateByAround = (vector, angle, axis) => vector.clone().applyAxisAngle(axis, angle);
+
+const angleFrom = (v1, v2, aroundAxis) => {
+    const angle = v1.angleTo(v2);
+    const crossed = cross(v1, v2);
+    const polarity = Math.sign(dot(aroundAxis, crossed));
+    return angle * polarity; // angleTo is absolute, doesn't include direction (sigh)
+}
